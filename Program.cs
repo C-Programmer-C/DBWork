3836cd11de3aa8b9866fd06c004944c0fca38aca
Singleton <misha2006metkig@gmail.com>
2025-03-03 10:21:46 +0500

    Add two commands Console.WriteLIne()

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ namespace DBWork
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AuthorizationForm());
+            Console.WriteLine();
+			Console.WriteLine();
+			Application.Run(new AuthorizationForm());
 
            
 		}
